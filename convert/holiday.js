029b084d08cbde69ac1f9220ac8005465461cd31
gentlable <shingo.gt.1115@gmail.com>
2023-02-03 11:19:48 +0900

    出荷予定日変更対応

--- convert/holiday.js
+++ convert/holiday.js
@@ -4,114 +4,142 @@
 
 const axios = require('axios');
 const _ = require('lodash');
-const moment = require('moment-timezone')
-moment.tz.setDefault("Asia/Tokyo")
+const moment = require('moment-timezone');
+moment.tz.setDefault('Asia/Tokyo');
 
 const API_KEY = 'AIzaSyBuDOB_9lGW99W-AbiH0uvJQkJq2fSQRuE';
 const ID = 'japanese__ja@holiday.calendar.google.com';
-const TARGET_MONTHS_START = 6
-const TARGET_MONTHS_END = 6
+const TARGET_MONTHS_START = 6;
+const TARGET_MONTHS_END = 6;
 
-let holidays = null
+let holidays = null;
 
 const getClosestWeekday = (date) => {
   if (date == undefined) {
-    date = moment().startOf('day')
+    date = moment().startOf('day').add(1, 'days');
   }
   while (includesMomentElement(holidays, date)) {
-    date.add(1, 'days')
+    date.add(1, 'days');
   }
-  return date
-}
+  return date;
+};
 
 const getEarlierOrSameWeekday = (date) => {
   while (includesMomentElement(holidays, date)) {
-    date.subtract(1, 'days')
+    date.subtract(1, 'days');
   }
-  return date
-}
+  return date;
+};
 
 const getNationalHolidays = async () => {
-  const from = moment().subtract(TARGET_MONTHS_START, 'months').format('YYYY-MM-DD') + 'T00:00:00Z'
-  const to = moment().add(TARGET_MONTHS_END, 'months').format('YYYY-MM-DD') + 'T00:00:00Z'
+  const from =
+    moment().subtract(TARGET_MONTHS_START, 'months').format('YYYY-MM-DD') +
+    'T00:00:00Z';
+  const to =
+    moment().add(TARGET_MONTHS_END, 'months').format('YYYY-MM-DD') +
+    'T00:00:00Z';
 
   // Googleに渡す日付を作成
   try {
-    const res = await axios.get(`https://www.googleapis.com/calendar/v3/calendars/${ID}/events?key=${API_KEY}&timeMin=${from}&timeMax=${to}`);
+    const res = await axios.get(
+      `https://www.googleapis.com/calendar/v3/calendars/${ID}/events?key=${API_KEY}&timeMin=${from}&timeMax=${to}`
+    );
     //console.log(res)
     return _.map(res.data.items, (item) => {
-      return moment(item.start.date)
+      return moment(item.start.date);
     });
   } catch (error) {
-    const {
-      status,
-      statusText
-    } = error.response;
-    alert(`API接続時にエラーが発生しました。HTTP Status: ${status} ${statusText}`)
-    throw error
+    const { status, statusText } = error.response;
+    alert(
+      `API接続時にエラーが発生しました。HTTP Status: ${status} ${statusText}`
+    );
+    throw error;
   }
 };
 
 const fetchHolidays = async () => {
   if (holidays != null) {
-    return
+    return;
   }
-  let holidaysTmp = await getNationalHolidays()
-  const from = moment().subtract(TARGET_MONTHS_START, 'months').startOf('day')
-  const to = moment().add(TARGET_MONTHS_END, 'months')
-  let date = from
+  let holidaysTmp = await getNationalHolidays();
+  const from = moment().subtract(TARGET_MONTHS_START, 'months').startOf('day');
+  const to = moment().add(TARGET_MONTHS_END, 'months');
+  let date = from;
   //国民の祝日+土日
   while (date.isBefore(to)) {
     //平日なら次のループへ
     if (date.day() != 6 && date.day() != 0) {
-      date.add(1, 'days')
-      continue
+      date.add(1, 'days');
+      continue;
     }
     if (!includesMomentElement(holidaysTmp, date)) {
-      holidaysTmp.push(date.clone())
+      holidaysTmp.push(date.clone());
     }
-    date.add(1, 'days')
+    date.add(1, 'days');
   }
   holidays = _.sortBy(holidaysTmp, function (holiday) {
-    return holiday.unix()
+    return holiday.unix();
   });
-}
+};
 
 const includesMomentElement = (arr, date) => {
   const existanse = _.find(arr, function (el) {
-    return el.isSame(date)
+    return el.isSame(date);
   });
-  return existanse !== undefined
-}
+  return existanse !== undefined;
+};
 
 const getShukkaYoteibi = (values) => {
-  let date = moment(values[1], 'YYYY/MM/DD')
+  let date = moment(values[1], 'YYYY/MM/DD');
   if (!date.isValid()) {
-    return getClosestWeekday(moment().startOf('day'))
+    return getClosestWeekday(moment().startOf('day').add(1, 'days'));
   }
-  console.log(values[0])
-  if ([
-    '北海道', '福岡県', '佐賀県', '大分県', '長崎県', '熊本県', '宮崎県', '鹿児島県', '沖縄県',
-    '青森県', '兵庫県', '岡山県', '広島県', '山口県', '鳥取県', '島根県', '徳島県', '香川県', '高知県', '愛媛県'
-  ]
-    .includes(values[0])) {
-    date.subtract(2, 'days')
+  console.log(values[0]);
+  if (
+    [
+      '北海道',
+      '福岡県',
+      '佐賀県',
+      '大分県',
+      '長崎県',
+      '熊本県',
+      '宮崎県',
+      '鹿児島県',
+      '沖縄県',
+      '青森県',
+      '兵庫県',
+      '岡山県',
+      '広島県',
+      '山口県',
+      '鳥取県',
+      '島根県',
+      '徳島県',
+      '香川県',
+      '高知県',
+      '愛媛県',
+    ].includes(values[0])
+  ) {
+    date.subtract(2, 'days');
   } else {
-    date.subtract(1, 'days')
+    date.subtract(1, 'days');
   }
-  return getEarlierOrSameWeekday(date)
-}
+  return getEarlierOrSameWeekday(date);
+};
 
 const validateShukkaYoteibi = (values) => {
-  let date = getShukkaYoteibi(values)
+  let date = getShukkaYoteibi(values);
   if (date.isBefore(moment(), 'day')) {
-    throw new Error(`お届け希望日から計算した出荷予定日が今日以前（${date.format('YYYY/MM/DD')}）になってしまいます。`)
+    throw new Error(
+      `お届け希望日から計算した出荷予定日が今日以前（${date.format(
+        'YYYY/MM/DD'
+      )}）になってしまいます。`
+    );
   }
-  return true
-}
+  return true;
+};
 
 module.exports = {
   getShukkaYoteibi,
   validateShukkaYoteibi,
   fetchHolidays,
-}
+};
--- convert/index.js
+++ convert/index.js
@@ -1,201 +1,228 @@
-const {app} = window.require('electron').remote
-const path = window.require('path')
-const moment = require('moment')
+const { app } = window.require('electron').remote;
+const path = window.require('path');
+const moment = require('moment');
 // const commander = require('commander')
 //   .option('-c, --config-file <type>')
 //   .option('-i, --input-csv <type>')
 //   .option('-o, --output-csv <type>')
 // commander.parse(process.argv)
-const configFilePath = __dirname + '/config.js'
-const config = require(configFilePath)
-const parseJson = config.inputSettings.json
+const configFilePath = __dirname + '/config.js';
+const config = require(configFilePath);
+const parseJson = config.inputSettings.json;
 
-const fileStream = window.require('fs')
-const csvParser = require('csv')
-const {juchuNumbers} = require('./global')
+const fileStream = window.require('fs');
+const csvParser = require('csv');
+const { juchuNumbers } = require('./global');
 
 // クーポンが出力された受注コードを格納
-let couponOutputlist = []
+let couponOutputlist = [];
 
 // 出力のロジック
 const exportCsv = (data) => {
-  juchuNumbers.splice(0, juchuNumbers.length)
-  csvParser.stringify(data, {
-    quoted: true
-  }, (error, rawOutput) => {
-    const fileName = 'EC受注データ_' + moment().format('YYYYMMDDHHmmss') + '.csv'
-    const outputCsv = path.join(app.getPath('downloads'), fileName)
-    const output = iconv.encode(rawOutput, "Shift_JIS")
-    fileStream.writeFile(outputCsv, output, (error) => {
-      //手抜き
-      if (error) {
-        alert('システムエラーが発生しました。')
-        console.log(error)
-      } else {
-        alert(fileName + 'を出力しました。')
-      }
-    })
-  })
-}
+  juchuNumbers.splice(0, juchuNumbers.length);
+  csvParser.stringify(
+    data,
+    {
+      quoted: true,
+    },
+    (error, rawOutput) => {
+      const fileName =
+        'EC受注データ_' + moment().format('YYYYMMDDHHmmss') + '.csv';
+      const outputCsv = path.join(app.getPath('downloads'), fileName);
+      const output = iconv.encode(rawOutput, 'Shift_JIS');
+      fileStream.writeFile(outputCsv, output, (error) => {
+        //手抜き
+        if (error) {
+          alert('システムエラーが発生しました。');
+          console.log(error);
+        } else {
+          alert(fileName + 'を出力しました。');
+        }
+      });
+    }
+  );
+};
 
 // 設定ファイルの読み込みロジックを適用する
 const convertRowCommon = (row, outputs) => {
-  const parses = config.inputSettings.parses
+  const parses = config.inputSettings.parses;
   Object.keys(parses).forEach((key) => {
-    const fromValue = row[key]
-    row[key] = parses[key](fromValue)
-  })
+    const fromValue = row[key];
+    row[key] = parses[key](fromValue);
+  });
 
-  const newRow = []
+  const newRow = [];
   // 列の生成
-  outputs.columns.forEach(column => {
-    let value = null
+  outputs.columns.forEach((column) => {
+    let value = null;
 
     if ('from' in column) {
-      value = Array.isArray(column.from) ?
-        column.from.map((from) => {
-          return row[from]
-        }) :
-        row[column.from]
+      value = Array.isArray(column.from)
+        ? column.from.map((from) => {
+            return row[from];
+          })
+        : row[column.from];
     }
 
     if ('convert' in column) {
-      value = column.convert(value)
+      value = column.convert(value);
       if (value != undefined) {
       }
     }
 
     if (!value && 'default' in column) {
-      value = column.default
-      if (value != undefined && value != "") {
+      value = column.default;
+      if (value != undefined && value != '') {
       }
     }
-    newRow.push(value)
-  })
+    newRow.push(value);
+  });
 
-  return newRow
-}
+  return newRow;
+};
 
 const convertRow = (row) => {
-  return convertRowCommon(row, config.outputSettings.outputs)
-}
+  return convertRowCommon(row, config.outputSettings.outputs);
+};
 
 const convertRowShipment = (row) => {
-  return convertRowCommon(row, config.outputSettings.outputsShipment)
-}
+  return convertRowCommon(row, config.outputSettings.outputsShipment);
+};
 
 const convertRowCoupon = (row) => {
-  return convertRowCommon(row, config.outputSettings.outputsCoupon)
-}
+  return convertRowCommon(row, config.outputSettings.outputsCoupon);
+};
 
 const convertRowTax = (row) => {
-  return convertRowCommon(row, config.outputSettings.outputsTax)
-}
+  return convertRowCommon(row, config.outputSettings.outputsTax);
+};
 // 入出力のロジック
 const convertCsv = (error, data) => {
-  console.log("data",data)
-  const errorMessages = []
+  console.log('data', data);
+  const errorMessages = [];
   // バリデーション
   data.forEach((row, rowIndex) => {
     config.validationSettings.forEach((setting) => {
-      field = setting.field
-      value = Array.isArray(field) ?
-        field.map((from) => {
-          return row[from]
-        }) :
-        row[field]
+      field = setting.field;
+      value = Array.isArray(field)
+        ? field.map((from) => {
+            return row[from];
+          })
+        : row[field];
       try {
-        setting.validation(value, field)
+        setting.validation(value, field);
       } catch (e) {
-        errorMessages.push(`${rowIndex + 2}行目：${e.message}`)
-        console.log(e)
+        errorMessages.push(`${rowIndex + 2}行目：${e.message}`);
+        console.log(e);
       }
-    })
-  })
+    });
+  });
   // エラーメッセージがある場合は表示して終了
   if (0 < errorMessages.length) {
-    alert(errorMessages.join('\n'))
-    return
+    alert(errorMessages.join('\n'));
+    return;
   }
   // バリデーション
-  let newData = data.map(convertRow)
-  const juchuNumbersOfShipment = []
-  const shipmentData = data.filter((row) => {
-    const shipment = row['送料']
-    if (shipment !== '' && shipment !== '0' && !juchuNumbersOfShipment.includes(row['受注コード'])) {
-      juchuNumbersOfShipment.push(row['受注コード'])
-      return true
-    }
-    return false
-  }).map(convertRowShipment)
+  let newData = data.map(convertRow);
+  const juchuNumbersOfShipment = [];
+  const shipmentData = data
+    .filter((row) => {
+      const shipment = row['送料'];
+      if (
+        shipment !== '' &&
+        shipment !== '0' &&
+        !juchuNumbersOfShipment.includes(row['受注コード'])
+      ) {
+        juchuNumbersOfShipment.push(row['受注コード']);
+        return true;
+      }
+      return false;
+    })
+    .map(convertRowShipment);
 
-  const couponData = data.filter((row) => {
+  const couponData = data
+    .filter((row) => {
+      if (
+        typeof row['割引合計金額（商品）'] === 'undefined' ||
+        row['割引合計金額（商品）'] == '' ||
+        isNaN(row['割引合計金額（商品）']) ||
+        row['割引合計金額（商品）'] === '0'
+      ) {
+        return false;
+      }
 
-    if (
-      typeof row['割引合計金額（商品）'] === 'undefined' ||
-      row['割引合計金額（商品）'] == '' ||
-      isNaN(row['割引合計金額（商品）']) ||
-      row['割引合計金額（商品）'] === '0'
-    ) {
-      return false
-    }
-  
-    // クーポン出力リストを照合
-    // 受注コードが存在する場合
-    if(couponOutputlist.includes(row['受注コード'])){
-      // クーポンデータを出力しない
-      return false
-    }
-    // 受注コードが存在しなかった場合
-    // クーポン出力リストに受注コードを追加
-    couponOutputlist.push(row['受注コード'])
+      // クーポン出力リストを照合
+      // 受注コードが存在する場合
+      if (couponOutputlist.includes(row['受注コード'])) {
+        // クーポンデータを出力しない
+        return false;
+      }
+      // 受注コードが存在しなかった場合
+      // クーポン出力リストに受注コードを追加
+      couponOutputlist.push(row['受注コード']);
 
-    return true
-  }).map(convertRowCoupon)
+      return true;
+    })
+    .map(convertRowCoupon);
 
-  const taxData = data.filter((row, i, self) => {
-    return self.findIndex((_row => {
-      return _row['受注コード'] === row['受注コード']
-    })) === i
-  }).map(convertRowTax)
+  const taxData = data
+    .filter((row, i, self) => {
+      return (
+        self.findIndex((_row) => {
+          return _row['受注コード'] === row['受注コード'];
+        }) === i
+      );
+    })
+    .map(convertRowTax);
 
-  const header = config.outputSettings.outputs.columns.map(column => column.name)
-  newData = newData.concat(shipmentData)
-  newData = newData.concat(couponData)
-  newData = newData.concat(taxData)
+  const header = config.outputSettings.outputs.columns.map(
+    (column) => column.name
+  );
+  newData = newData.concat(shipmentData);
+  newData = newData.concat(couponData);
+  newData = newData.concat(taxData);
   newData.sort((row1, row2) => {
     if (row1[header.indexOf('受注№')] !== row2[header.indexOf('受注№')]) {
-      return Number(row1[header.indexOf('受注№')]) - Number(row2[header.indexOf('受注№')])
+      return (
+        Number(row1[header.indexOf('受注№')]) -
+        Number(row2[header.indexOf('受注№')])
+      );
     }
     //同じ受注コードであれば課税区分が9の行（消費税の行）が一番後ろにソートされる
     if (Number(row1[header.indexOf('課税区分')]) == 9) {
-      return 1
+      return 1;
     }
     if (Number(row2[header.indexOf('課税区分')]) == 9) {
-      return -1
+      return -1;
     }
     //その他は行の値でソート
-    return row1[header.indexOf('行')] < row2[header.indexOf('行')]
-  })
-  newData.unshift(header)
-  exportCsv(newData)
-}
+    return row1[header.indexOf('行')] < row2[header.indexOf('行')];
+  });
+  newData.unshift(header);
+  exportCsv(newData);
+};
 
-const iconv = require("iconv-lite")
+const iconv = require('iconv-lite');
 
 const executeCsvConvert = (filePath) => {
-
   // クーポン出力リストを初期化
-  couponOutputlist = []
+  couponOutputlist = [];
+
+  console.log('filePath', filePath);
 
   fileStream
     .createReadStream(filePath)
     .pipe(iconv.decodeStream('Shift_JIS'))
-    .pipe(csvParser.parse({
-      columns: parseJson
-    }, convertCsv))
-}
+    .pipe(
+      csvParser.parse(
+        {
+          columns: parseJson,
+        },
+        convertCsv
+      )
+    );
+};
 
 module.exports = {
   executeCsvConvert,
-}
+};
